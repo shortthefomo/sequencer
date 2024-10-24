06e736047cb875a2a1fbc946f4f671672eae0c00
Lathan Britz <jucallme@gmail.com>
2024-10-24 13:17:41 -0400

    remove color

--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -149,7 +149,7 @@ html {
   margin-left: 1px;
 }
 .validated {
-  transition: border-color 2s ease;
+//   transition: border-color 2s ease;
   border: solid 1px #00e56a;
 }
 .address {
@@ -165,7 +165,7 @@ html {
 .validated.address {
   background-color: orange;
   border: solid 1px orange;
-  transition: background-color 0.5s ease;
+//   transition: background-color 0.5s ease;
 }
 
 .btn-outline-pink {
